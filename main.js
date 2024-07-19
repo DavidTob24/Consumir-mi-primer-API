c88a932e9cf4eb9e6dc4282d09c626558d32b4ec
David_Tobar <tobar.jonathan@correounivalle.edu.co>
2024-07-19 05:21:38 -0500

    Agregando funcionalidades

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const API_KEY = 'live_BbbW8c9j31q4eM5Jqycu84BOB15VXdfKHyNw1kDUko3YEeToILaf2Gk7JI
 const API_URL_RANDOM = `https://api.thedogapi.com/v1/images/search?limit=2&api_key=${API_KEY}`;
 const API_URL_FAV = `https://api.thedogapi.com/v1/favourites?limit=40&api_key=${API_KEY}`;
 const API_URL_FAV_DEL = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=${API_KEY}`;
+const API_URL_upload = 'https://api.thedogapi.com/v1/images/upload'
+
 
 const spanError = document.getElementById('error');
 
@@ -39,35 +41,33 @@ const loadFavoriteDogs = async () => {
         if (res.status !== 200) {
             spanError.innerHTML = `Hubo un error cargando favoritos: ${res.status} ${res.statusText}`;
             return;
+        }else{
+            const section = document.getElementById('favoriteDogs');
+            section.innerHTML = "";
+
+            const h2 = document.createElement('h2');
+            h2.textContent = 'Perritos favoritos';
+            section.appendChild(h2);
+
+            console.log('FAVOURITES', data);
+
+
+            data.forEach(dog => {
+                const article = document.createElement('article');
+                const img = document.createElement('img');
+                const button = document.createElement('button');
+                const btntext = document.createTextNode('Sacar perrito de favs');
+
+                img.className = 'fav_img'
+                img.src = dog.image.url;
+                button.appendChild(btntext);
+                button.onclick = () => deleteFavouriteDog(dog.id);
+                article.appendChild(img);
+                article.appendChild(button);
+                section.appendChild(article);
+            });
         }
 
-        const section = document.getElementById('favoriteDogs');
-        section.innerHTML = "";
-
-        const h2 = document.createElement('h2');
-        h2.textContent = 'Perritos favoritos';
-        section.appendChild(h2);
-
-        console.log('FAVOURITES', data);
-
-        const fragment = document.createDocumentFragment();
-
-        data.forEach(dog => {
-            const article = document.createElement('article');
-            const img = document.createElement('img');
-            const button = document.createElement('button');
-            const btntext = document.createTextNode('Sacar perrito de favs');
-
-            img.src = dog.image.url;
-            img.width = 150;
-            button.appendChild(btntext);
-            button.onclick = () => deleteFavouriteDog(dog.id);
-            article.appendChild(img);
-            article.appendChild(button);
-            fragment.appendChild(article);
-        });
-
-        section.appendChild(fragment);
     } catch (error) {
         spanError.innerHTML = `Hubo un error cargando favoritos: ${error.message}`;
     }
@@ -120,6 +120,39 @@ const deleteFavouriteDog = async (id) => {
 const refreshContent = () => {
     loadRandomDogs();
 };
+
+console.log("main.js está cargado correctamente");
+
+const uploadDogimg = async () => {
+    const form = document.getElementById('uploading-form');
+    const formData = new FormData(form);
+
+    try {
+        const res = await fetch(API_URL_upload, {
+            method: 'POST',
+            headers: {
+                'X-API-KEY': API_KEY
+            },
+            body: formData,
+        });
+
+        if (res.status !== 201) {
+            spanError.innerHTML = `Hubo un error subiendo la imagen: ${res.status} ${res.statusText}`;
+            return;
+        }
+
+        const data = await res.json();
+        console.log('Imagen subida:', data);
+        console.log(data.url)
+
+        saveFavouriteDog(data.id); // Guarda la imagen subida como favorita
+
+    } catch (error) {
+        spanError.innerHTML = `Hubo un error subiendo la imagen: ${error.message}`;
+    }
+};
+
+
 
 loadRandomDogs();
 loadFavoriteDogs();
